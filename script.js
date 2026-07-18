81c62c1e4426ab41cf1e88cfb94eabc868948301
eritho91 <erik.thorell@iths.se>
2026-07-18 15:34:45 +0200

    Remove fade-in effect from script.js
    Removed fade-in effect code and retained print functionality.

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // 1. Mjuk fade-in effekt
-    document.body.classList.add('fade-in');
-
-    // 2. Print-funktionalitet
+    // Print-funktionalitet
     const printBtn = document.getElementById("printButton");
     if (printBtn) {
         printBtn.addEventListener("click", () => {
